c110575d13c141f61d2683649795ee778d774752
JGWill <jgi@jgwill.com>
2018-11-09 18:10:13 -0500

    import OpTair code

---
+++ lib180224.ino
@@ -0,0 +1,17 @@
+//---------------------------------
+//--------- OptAiR --------
+//----JGOptAIR__Unified_v1_180209
+//---------- A Quick merge of Smoke/Humider and day to test.... (before the more complex merge)
+/* Optimal Home Automation and Fun / OptAir
+ *  
+
+
+A Library usable by many Prototype / Module of OptAir
+ *    
+
+*/
+
+
+
+
+//This library would be abstraction of common function such as VENT_ON(), HUMIDFY_ON()...
